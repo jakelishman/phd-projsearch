f1c4573ca56be6807f5d53c961bb5acc31adb6ef
Jake Lishman <jakelishman@gmail.com>
2018-03-15 10:50:31 +0000

    Move state functions to iontools module

--- target.py
+++ target.py
@@ -8,31 +8,6 @@ def _named_field(array, field):
     the field `field`."""
     return array.getfield(*array.dtype.fields[field])
 
-def qubit_projectors(target):
-    """qubit_projectors(target: qutip.Qobj | int) -> qutip.Qobj * qutip.Qobj
-
-    Given either an example state (2-level system tensor Fock basis) or a number
-    of motional states to be considered in the Fock basis, return a pair of the
-    projectors onto the "e" and "g" subspaces respectively."""
-    ns = target.dims[0][1] if isinstance(target, qutip.Qobj) else target
-    return (qutip.tensor(qutip.basis(2, 0), qutip.qeye(ns)).dag(),
-            qutip.tensor(qutip.basis(2, 1), qutip.qeye(ns)).dag())
-
-def populated_elements(state):
-    """populated_elements(state: qutip.Qobj) -> np.array of string * complex
-
-    Given a state vector of a 2-level system tensor a Fock state, return a list
-    of (name * value) pairs for each of the populated states.  `name` is a
-    string of the qubit followed by the Fock level (e.g. "g0" or "e10"), and
-    `value` is the complex value that was in the state."""
-    digits = len(str(state.dims[0][1] - 1))
-    return np.array([
-        (qubit + str(i), val) \
-        for qubit, proj in zip(["e", "g"], qubit_projectors(state)) \
-        for i, val in enumerate((proj * state).full().flat) \
-        if abs(val) > 1e-11
-    ], dtype=np.dtype([("element", "U{}".format(digits+1)), ("value", "c16")]))
-
 def linear_independent_set(state):
     """linear_independent_set(state: qutip.Qobj) -> np.array of qutip.Qobj
 
@@ -47,7 +22,7 @@ def linear_independent_set(state):
     the original input span the complex space defined as
         c_0 |g0> + c_1 |e2> + c_2 |e3>
     with {c_i} all complex."""
-    populated = populated_elements(state)
+    populated = it.state.populated_elements(state)
     ns = it.state.ns(state)
     return np.array([it.state.create({el: 1}, ns=ns)
                      for el in _named_field(populated[1:], "element")])
@@ -60,7 +35,7 @@ def orthonormal_basis(state):
     the returned array will always be the normalised version of the input state.
     """
     ns = it.state.ns(state)
-    populated = populated_elements(state)
+    populated = it.state.populated_elements(state)
     els = _named_field(populated, "element")
     values = _named_field(populated, "value")
     others = [it.state.element(ket, els)
@@ -106,7 +81,7 @@ def target(states, sequence):
         pulses in sequence, i.e. [time, phase, time, phase, ...]) and returns
         the value of the infidelity of the gate operation and its derivatives
         with respect to each of the parameters."""
-    e_bra, g_bra = qubit_projectors(states[0])
+    e_bra, g_bra = it.state.qubit_projectors(states[0])
     bras = np.array([g_bra] + [e_bra] * (len(states) - 1))
     def func(params):
         op = sequence.op(params)
@@ -114,7 +89,7 @@ def target(states, sequence):
         for i in range(len(states) - 1):
             infid += (e_bra * op * states[i + 1]).norm() ** 2
         deriv = np.zeros_like(params)
-        op_proj = bras * sequence.op(params) * states
+        op_proj = bras * op * states
         for (i, d_op) in enumerate(sequence.d_op(params)):
             for (oper, d_oper) in zip(op_proj, bras * d_op * states):
                 deriv[i] += 2 * np.real((oper.dag() * d_oper).data[0, 0])

cc82ace122887fbd578242fbb0cb0bcd7688fcdc
Jake Lishman <jakelishman@gmail.com>
2018-04-10 15:19:55 +0100

    Add some additional functionals

--- functional.py
+++ functional.py
@@ -2,6 +2,8 @@
 
 import itertools
 
+__all__ = ['exists', 'skip', 'tuples', 'flatmap', 'funnelmap']
+
 def exists(predicate, iterable):
     """exists(predicate: 'T -> bool, iterable: iter of 'T) -> 'T
 
@@ -33,3 +35,31 @@ def tuples(iterable, n=2):
         tuples(range(3), 2) -> [ (0, 1), (1, 2) ]
     and so on."""
     return zip(*map(lambda t: skip(*t), enumerate(itertools.tee(iterable, n))))
+
+def flatmap(mapping, iterable):
+    """flatmap(mapping: 'A -> iterable of 'B, iterable: iterable of 'A)
+    -> iterable of 'B
+
+    Applies `mapping` to each element of `iterable`, flattening out returned
+    iterators."""
+    for el in iterable:
+        yield from mapping(el)
+
+class funnelmap:
+    """funnelmap(consumer: iterator of 'A -> 'B, iterable: iterable of 'A)
+    -> iterable of 'B
+
+    Creates a new iterable using the `consumer` function on the given
+    `iterable`.  The consumer function may consume as many values as it likes
+    from the base iterable, and then return a value.  For each subsequent value,
+    the underlying iterable continues from the place it was left off at.
+
+    The `consumer` function may raise `StopIteration` which will terminate the
+    iteration even if the iterator itself is not exhausted."""
+    def __init__(self, consumer, iterable):
+        self._consumer = consumer
+        self._iterator = iter(iterable)
+    def __next__(self):
+        return self._consumer(self._iterator)
+    def __iter__(self):
+        return self
